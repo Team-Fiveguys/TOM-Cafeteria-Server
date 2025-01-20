547923c0be19945208543d6563ed09fa0d82d1c7
wuseong <jws001228@naver.com>
2025-01-20 22:01:57 +0900

    refactor: 메뉴 엔티티 설정 수정, 연관관계 메서드 추가

--- src/main/java/fiveguys/Tom/Cafeteria/Server/domain/menu/entity/Menu.java
+++ src/main/java/fiveguys/Tom/Cafeteria/Server/domain/menu/entity/Menu.java
@@ -2,13 +2,17 @@ package fiveguys.Tom.Cafeteria.Server.domain.menu.entity;
 
 import fiveguys.Tom.Cafeteria.Server.domain.cafeteria.entity.Cafeteria;
 import fiveguys.Tom.Cafeteria.Server.domain.common.BaseEntity;
+import fiveguys.Tom.Cafeteria.Server.domain.diet.entity.MenuDiet;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.List;
 
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 @Entity
 public class Menu extends BaseEntity {
     @Id
@@ -21,11 +25,24 @@ public class Menu extends BaseEntity {
 
     private int likeCount;
 
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "cafeteria_id")
     private Cafeteria cafeteria;
 
     @JoinColumn(name = "menu_category_id")
     @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private MenuCategory menuCategory;
+
+    @OneToMany(mappedBy = "menu", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<MenuDiet> menuDietList;
+
+    public void clearMenuDiet(){
+        for (MenuDiet menuDiet: this.menuDietList) {
+            menuDiet.clearMenu();
+        }
+        menuDietList.clear();
+    }
+    public void clearCafeteria(){
+        this.cafeteria = null;
+    }
 }
